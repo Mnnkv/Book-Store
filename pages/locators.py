7fc39688fd6593e2191491c4380d9567bd7b2eee
Alexey <117898732+Mnnkv@users.noreply.github.com>
2023-02-17 14:28:15 +0600

    New Page Object created New locators, page and test to check added product to cart

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,21 @@
 from selenium.webdriver.common.by import By
 
+
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    ADD_TO_CART = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+    CART = (By.CSS_SELECTOR, "#default > header > div.page_inner > div > div.basket-mini.pull-right.hidden-xs > span > a")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > h1")
+    PRODUCT_NAME_CART = (By.CSS_SELECTOR, "h3 > a")
+    PRICE = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color")
+    TOTAL = (By.CSS_SELECTOR, "#basket_totals > table > tbody > tr:nth-child(2) > th.total.align-right")
+
+
---
+++ pages/product_page.py
@@ -0,0 +1,22 @@
+from .locators import ProductPageLocators
+from .base_page import BasePage
+
+
+class ProductPage(BasePage):
+    def product_added(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        product_price = self.browser.find_element(*ProductPageLocators.PRICE).text
+        add_to_cart = self.browser.find_element(*ProductPageLocators.ADD_TO_CART)
+        add_to_cart.click()
+        self.solve_quiz_and_get_code()
+        self.browser.find_element(*ProductPageLocators.CART).click()
+        product_name_cart = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_CART).text
+        total_price = self.browser.find_element(*ProductPageLocators.TOTAL).text
+        assert product_name == product_name_cart, "Product name is wrong!"
+        assert product_price == total_price, "Price is wrong!"
+
+
+
+
+
+
